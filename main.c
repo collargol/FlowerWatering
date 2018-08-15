f48838d058ad28e7001bc3b82202ad5dce0ef5aa
collargol <michal.trojnarski95@gmail.com>
2018-08-16 00:05:05 +0200

    Unused functions and macros removed

--- main.c
+++ main.c
@@ -9,16 +9,12 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-//#include <avr/interrupt.h>
 
-#define REF_AVCC (0 << REFS0)
-#define REF_INT	 (1 << REFS0)
 #define LEDY PB4
 #define LEDG PB1
 #define ADC_PORT 1
 
-void displayValue(uint8_t value);
-void initADC();
+//void initADC();
 uint16_t readFromADC(uint8_t adc_num);
 
 int main(void)
@@ -27,39 +23,34 @@ int main(void)
 	uint16_t readedValue = 0;
 	//initADC();
 
-	
     while (1) 
     {
 		readedValue = readFromADC(ADC_PORT) / 10.24;
-		uint8_t val = 99 - (readedValue & 0xFF);
-		if (val < 30)
+		uint8_t percentValue = 99 - (readedValue & 0xFF);
+	
+		PORTB &= ~(1 << LEDY);
+		PORTB &= ~(1 << LEDG);
+
+		if (percentValue < 30)
 		{
-			PORTB &= ~(1 << LEDY);
-			PORTB &= ~(1 << LEDG);
 			PORTB |= (1 << LEDY) | (1 << LEDG);
 		}
-		if ((val >= 30) && (val < 80))
+		else if (percentValue < 90)
 		{
-			PORTB &= ~(1 << LEDY);
-			PORTB &= ~(1 << LEDG);
 			PORTB |= (1 << LEDY);
 		}
-		if (val >= 80)
+		else
 		{
-			PORTB &= ~(1 << LEDY);
-			PORTB &= ~(1 << LEDG);
 			PORTB |= (1 << LEDG);
 		}
     }
 }
 
-
-
-void initADC()
-{
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	// 128 prescale for 8MHz
-	ADCSRA |= (1 << ADEN);	// enable ADC
-}
+// void initADC()
+// {
+// 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	// 128 prescale for 8MHz
+// 	ADCSRA |= (1 << ADEN);	// enable ADC
+// }
 
 uint16_t readFromADC(uint8_t adc_num)
 {
@@ -70,12 +61,12 @@ uint16_t readFromADC(uint8_t adc_num)
 	ADMUX &= ~(1 << ADLAR); // right-adjusted
 	ADCSRA |= (1 << ADPS1) | (1 << ADPS0);	// 128 prescale for 8MHz
 	ADCSRA |= (1 << ADEN);	// enable ADC
+
 	ADCSRA |= (1 << ADSC);	// starting conversion
 	while (ADCSRA & (1 << ADSC)) {}	// wait until conversion is over
 
-	
 	readed_value = ADCL;
-	readed_value = (ADCH << 8) + readed_value;
+	readed_value += (ADCH << 8);
 
 	return readed_value;
 }
